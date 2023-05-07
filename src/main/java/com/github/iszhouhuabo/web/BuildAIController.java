01a52b4edcee8caed8d2adc4df1e31ddfadf82ae
iszhouhuabo <iszhouhuabo@foxmail.com>
2023-05-07 21:23:56 +0800

    feat: add build AI

---
+++ src/main/java/com/github/iszhouhuabo/web/BuildAIController.java
@@ -0,0 +1,17 @@
+package com.github.iszhouhuabo.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 构建模型
+ *
+ * @author louye
+ */
+@RestController
+@RequestMapping("api/build")
+@RequiredArgsConstructor
+public class BuildAIController {
+    
+}
