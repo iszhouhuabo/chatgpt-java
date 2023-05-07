e9bd28aae81a30d8ed63d64b7dfe6ce86ff286e4
iszhouhuabo <iszhouhuabo@foxmail.com>
2023-05-07 21:22:55 +0800

    feat: 去掉默认的前端页面 HTML

--- src/main/java/com/github/iszhouhuabo/ChatApplication.java
+++ src/main/java/com/github/iszhouhuabo/ChatApplication.java
@@ -3,7 +3,6 @@ package com.github.iszhouhuabo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author louye
@@ -13,15 +12,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
-    }
-
-    @GetMapping("")
-    public String index() {
-        return "1.html";
-    }
-
-    @GetMapping("/websocket")
-    public String websocket() {
-        return "websocket.html";
     }
 }
